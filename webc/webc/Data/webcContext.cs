d89c59838fdede36a81898368b8371ce1fa3fc35
giray <giray@DESKTOP-U46VAKS>
2025-01-11 14:39:58 +0300

    Add project files.

---
+++ webc/webc/Data/webcContext.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Pok;
+
+namespace webc.Data
+{
+    public class webcContext : DbContext
+    {
+        public webcContext (DbContextOptions<webcContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Pok.Pokemon> Pokemon { get; set; } = default!;
+        public DbSet<Pok.Region> Region { get; set; } = default!;
+        public DbSet<Pok.Item> Item { get; set; } = default!;
+        public DbSet<Pok.Trainer> Trainer { get; set; } = default!;
+        public DbSet<Pok.Move> Move { get; set; } = default!;
+        public DbSet<Pok.USES> USES { get; set; } = default!;
+        public DbSet<Pok.Carries> Carries { get; set; } = default!;
+        public DbSet<Pok.Evolution> Evolution { get; set; } = default!;
+    }
+}
---
+++ webc/webc/Program.cs
@@ -0,0 +1,57 @@
+﻿using webc.Client.Pages;
+using webc.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using webc.Data;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Radzen;
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<webcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("webcContext") ?? throw new InvalidOperationException("Connection string 'webcContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+
+
+builder.Services.AddScoped<ProtectedLocalStorage>();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<DialogService>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(webc.Client._Imports).Assembly);
+
+
+app.Run();
